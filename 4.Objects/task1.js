12aa6589f17e7a6d0c7ff32fbae63638cce3d602
ferz1091 <ferz1091@gmail.com>
2022-05-18 12:28:56 +0300

    Solutions for 4.Objects

--- 4.Objects/task1.js
+++ 4.Objects/task1.js
@@ -6,18 +6,20 @@
  * Если до конца месяца осталось больше чем 20 дней — возвращается строка `good salary`, а если нет — `bad salary`
  */
 
-const person = {
-    salary() {
+const person = {};
+
+Object.defineProperty(person, 'salary', {
+    get() {
         const today = new Date();
         const year = today.getFullYear();
         const month = today.getMonth() + 1;
         const day = today.getDate();
         return new Date(year, month, 0).getDate() - day > 20 ? 'good salary' : 'bad salary';
     }
-};
+})
 
 // Решение
 
-console.log(person.salary()); // good salary
+console.log(person.salary); // good salary
 
 exports.person = person;
