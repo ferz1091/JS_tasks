c0ad7f67d3f7439fd026873bd3d7a47a1d7aa697
ferz1091 <ferz1091@gmail.com>
2022-05-19 17:05:36 +0300

    Solutions for 9.Improved_objects_capabilities

---
+++ 9.Improved_objects_capabilities/task1.js
@@ -0,0 +1,38 @@
+/**
+ * Задача 1.
+ *
+ * Создайте функцию shallowMerge.
+ * Функция обладает двумя параметрами, каждый из которых должен быть обычным JavaScript объектом.
+ * Функция возвращает новый объект, который в себе объединяет свойства обоих объектов.
+ * Свойства необходимо копировать лишь на один уровень каждого их объектов.
+ *
+ * Из объектов и обеих аргументах копируются только собственные свойства, включая недоступные для перечисления.
+ *
+ * Условия:
+ * - Встроенный метод Object.create() использовать запрещено;
+ * - При копировании любого свойства необходимо обязательно сохранить его дескрипторы;
+ * - Свойства с одинаковыми именами нужно перезаписывать — приоритетом обладает объект из второго параметра.
+ */
+
+// function shallowMerge(obj1, obj2) {
+//     console.log(Object.getOwnPropertyDescriptor(obj1, 'firstName'));
+// }
+
+function shallowMerge(a, b) {
+    if (arguments.length !== 2) throw new Error('Must be 2 arguments');
+    if (typeof a !== 'object' || typeof b !== 'object') throw new Error('Must be objects');
+    if (a === b) throw new Error('It is the same object');
+    return Object.defineProperties({...a, ...b}, { ...Object.getOwnPropertyDescriptors(a), ...Object.getOwnPropertyDescriptors(b) });
+}
+// Решение
+
+const user = { firstName: 'Marcus', lastName: 'Kronenberg' };
+const userData = { job: 'developer', country: 'Germany', lastName: 'Schmidt' };
+
+Object.defineProperty(user, 'firstName', { writable: false });
+Object.defineProperty(userData, 'job', { configurable: false });
+
+const result = shallowMerge(user, userData);
+console.log(result); // { firstName: 'Marcus', lastName: 'Schmidt', job: 'developer', country: 'Germany' }
+console.log(Object.getOwnPropertyDescriptor(result, 'firstName').writable); // false
+console.log(Object.getOwnPropertyDescriptor(result, 'job').configurable); // false
